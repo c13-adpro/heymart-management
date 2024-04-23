8a23eae3f1b72abfc8983d4ba494fa45db4f8545
juankhusuma <66712975+juankhusuma@users.noreply.github.com>
2024-04-24 01:14:32 +0700

    Setup for deployment

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,32 @@
-FROM rust:1.74.1
+# Start with a rust alpine image
+FROM rust:1-alpine3.15 AS builder
+
+# This is important, see https://github.com/rust-lang/docker-rust/issues/85
+ENV RUSTFLAGS="-C target-feature=-crt-static"
+
+# if needed, add additional dependencies here
+RUN apk add --no-cache musl-dev
+
+# set the workdir and copy the source into it
+WORKDIR /app
+COPY ./ /app
+
+# do a release build
+RUN cargo build --release
+RUN strip target/release/rocket-app
+
+# use a plain alpine image, the alpine version needs to match the builder
+FROM alpine:3.15
+
+# if needed, install additional dependencies here
+RUN apk add --no-cache libgcc
+
+COPY --from=builder /app/target/release/rocket-app /usr/local/bin/
 
 ENV ROCKET_ADDRESS=0.0.0.0
 ENV ROCKET_PORT=8080
 
-WORKDIR /app
-COPY . .
+WORKDIR /root
 
-RUN rustup default nightly
-RUN cargo build
-
-EXPOSE 8080
-CMD ["cargo", "run"]
+# set the binary as entrypoint
+CMD /usr/local/bin/rocket-app
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+use controller::route_stage;
+use rocket::tokio;
+use std::env;
+
 #[macro_use]
 extern crate rocket;
 pub mod controller;
@@ -6,41 +10,24 @@ pub mod model;
 pub mod repository;
 pub mod service;
 
-use controller::route_stage;
 use dotenvy::dotenv;
-use rocket_cors::AllowedOrigins;
-use shuttle_rocket::ShuttleRocket;
 use sqlx::postgres::PgPoolOptions;
 
-#[shuttle_runtime::main]
-async fn main(#[shuttle_runtime::Secrets] secrets: shuttle_runtime::SecretStore) -> ShuttleRocket {
-    let db_url = secrets.get("DATABASE_URL").unwrap();
-    secrets.into_iter().for_each(|(key, value)| {
-        std::env::set_var(key, value);
-    });
-
+#[tokio::main]
+async fn main() {
     dotenv().ok();
-    println!("DATABASE_URL: {}", db_url);
+    let db_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set.");
 
     let pool = PgPoolOptions::new()
-        // .max_connections(10)
+        .max_connections(10)
         .connect(&db_url)
         .await
         .unwrap();
 
-    let allowed_origins = AllowedOrigins::some_exact(&["*"]);
-
-    let cors = rocket_cors::CorsOptions {
-        allowed_origins,
-        ..Default::default()
-    }
-    .to_cors()
-    .unwrap();
-
-    let server = rocket::build()
+    rocket::build()
         .manage(pool)
-        .attach(cors)
-        .attach(route_stage());
-
-    Ok(server.into())
+        .attach(route_stage())
+        .launch()
+        .await
+        .unwrap();
 }
