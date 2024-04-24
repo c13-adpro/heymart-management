71f652eac4f9e2ff86a2f0e6d501676941b58144
shabhias <shabhialiyya3101@gmail.com>
2024-04-24 21:31:28 +0700

    [GREEN] implement product repository: create

--- src/main/java/id/ac/ui/cs/advprog/heymartmanagement/repository/ProductRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/heymartmanagement/repository/ProductRepository.java
@@ -4,7 +4,17 @@ import java.util.Map;
 import id.ac.ui.cs.advprog.heymartmanagement.model.Product;
 
 public class ProductRepository {
-   
+    private final Map<String, Product> products;
 
+    public ProductRepository() {
+        this.products = new HashMap<>();
+    }
 
+    public void create(Product product) {
+        products.put(product.getProductId(), product);
+    }
+
+    public Product findByProductId(String productId) {
+        return products.get(productId);
+    }
 }
